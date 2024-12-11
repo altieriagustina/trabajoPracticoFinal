29f9c40772f35599cf2648dfdbeeddecf3ddf39e
altieriagustina <altieriagustina@gmail.com>
2024-12-11 18:26:44 -0300

    Página Web final

--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,16 @@
 let articulos = document.getElementById("frutos-secos")
 let totalText = document.getElementById("totalText")
+let btnPay = document.getElementById("btnPay")
 
 const articles = [
     {
-        producto: "Maní sin sal", descripcion: `El maní sin sal es un fruto seco natural, rico en proteínas, grasas saludables y fibra. Ofrece un sabor suave y cremoso, ideal para consumir como snack, en recetas o como complemento nutritivo en una dieta balanceada.`, precio: 150000, stock: 10, path_img: `../IMAGES/image1.png`,
+        producto: "Maní sin sal", descripcion: `El maní sin sal es un fruto seco natural, rico en proteínas, grasas saludables y fibra. Ofrece un sabor suave y cremoso, ideal para consumir como snack, en recetas o como complemento nutritivo en una dieta balanceada.`, precio: 600, stock: 20, path_img: `../IMAGES/image1.png`,
     },
     {
-        producto: "Nueces", descripcion: `Las nueces son frutos secos con un sabor suave y mantecoso, reconocidos por su alto contenido de ácidos grasos omega-3, antioxidantes y fibra. Son ideales como snack, en ensaladas, postres o como ingrediente en recetas saludables.`, precio: 140000, stock: 8, path_img: `../IMAGES/image2.png`,
+        producto: "Nueces", descripcion: `Las nueces son frutos secos con un sabor suave y mantecoso, reconocidos por su alto contenido de ácidos grasos omega-3, antioxidantes y fibra. Son ideales como snack, en ensaladas, postres o como ingrediente en recetas saludables.`, precio: 2500, stock: 30, path_img: `../IMAGES/image2.png`,
     },
     {
-        producto: "Almendras", descripcion: `Las almendras son frutos secos crujientes y nutritivos, ricos en vitamina E, proteínas y grasas saludables. Su sabor suave y ligeramente dulce las hace perfectas como snack, en repostería o para enriquecer recetas saludables.`, precio: 200000, stock: 4, path_img: `./IMAGES/image3.png`,
+        producto: "Almendras", descripcion: `Las almendras son frutos secos crujientes y nutritivos, ricos en vitamina E, proteínas y grasas saludables. Su sabor suave y ligeramente dulce las hace perfectas como snack, en repostería o para enriquecer recetas saludables.`, precio: 3500, stock: 30, path_img: `./IMAGES/image3.png`,
     },
 ]
 
@@ -20,7 +21,8 @@ function pintarArticulos(arrayProductos) {
                 <img src="${articles[i].path_img}" alt="Imagen de Fruto Seco">
                 <h3 class="title">${articles[i].producto}</h3>
                 <p class="description">${articles[i].descripcion} </p>
-                <h4 class="precio">Precio: $ ${articles[i].precio} </h4>
+                <h4 class="precio">Precio por 100gr: $ ${articles[i].precio} </h4>
+                <p class="stock"> Stock</p>
                 <input type="number" id="stock${i}" value="${arrayProductos[i].stock}" readonly>
                 <input type="number" id="entrada${i}" placeholder = "Ingrese cantidad">
                 <button class="btnAddToCart" id="btnAddToCart${i}">Agregar al carrito</button>
@@ -47,10 +49,16 @@ function comprar(index) {
         total += cantidad * precio;
         totalText.innerHTML = `Total en su carrito: $ ${total}`;
         stockElement.value = stock - cantidad;
-        alert(`Compra realizada con éxito. Total: $${total}`);
+        alert(`Compra realizada con éxito. Total: $ ${total}`);
     } else {
-        alert(`Cantidad no válida, debe ser mayor que 0 y menor o igual al stpck disponible`);
+        alert(`Cantidad no válida, debe ser mayor que 0 y menor o igual al stock disponible`);
     }
 }
 
 pintarArticulos(articles)
+
+btnPay.addEventListener("click", finalizarCompra);
+
+function finalizarCompra () {
+    alert(`Compra finalizada con éxito, será redirigido a la plataforma de pago.`)
+}
